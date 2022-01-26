c4ebe22ec64acd2c7f74bd6617311131b520bc36
MaximSecor <72449272+MaximSecor@users.noreply.github.com>
2022-01-25 20:34:48 -0500

    Update gausub.py

--- ycc/gausub.py
+++ ycc/gausub.py
@@ -68,6 +68,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Submit Gaussian 16 job on Grace.')
     parser.add_argument("infile", help="Gaussian 16 input file")
     parser.add_argument("-nt", "--nthreads",metavar='N', help="CPUs-per-node",type=int,default=1)
+    parser.add_argument("-p","--partition",metavar='P',help="queue",type=int,default='day')
+    parser.add_argument("-r","--reservation",metavar='R',help="reservation",type=int,default='chem496')
     parser.add_argument("-t","--time",metavar='T',help="time (min)",type=int,default=15)
     args = parser.parse_args()
 
